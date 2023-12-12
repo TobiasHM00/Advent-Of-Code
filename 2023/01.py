5d6bd9c932b7d16b0c602e583a0a77cd9f1ae582
Tobias <tobiashagnessmortensen@gmail.com>
2023-12-13 00:26:28 +0100

    1a done

--- 2023/01.py
+++ 2023/01.py
@@ -1,1 +1,28 @@
-print("hello world")
+def a():
+    f = open("input01.txt", "r")
+    sum = 0
+    for line in f:
+        l = line.strip()
+        num = ""
+        for c in l:
+            if c.isnumeric():
+                num += c
+                break
+        
+        for c in reversed(l):
+            if c.isnumeric():
+                num += c
+                break
+        
+        sum += int(num)
+    
+    print(sum)
+    f.close()
+    
+a()
+
+def b():
+    numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    print(1)
+    
+b()
