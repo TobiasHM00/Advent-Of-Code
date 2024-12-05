3ce25976a324dc57d940647ac75b1847f3717d3a
Tobias <tobiashagnessmortensen@gmail.com>
2024-12-05 12:50:59 +0100

    ferdig med dag 3, feilen var at scanneren hadde lest gjennom inputen før den kom til del2 så bare flyttet det inn under del1 koden

--- 2024/03/tredje.java
+++ 2024/03/tredje.java
@@ -15,11 +15,17 @@ class tredje {
 
         String rx = "mul\\((\\d{1,3}),(\\d{1,3})\\)";
         Pattern p = Pattern.compile(rx);
+        
+        String rx2 = "(?:mul\\((\\d{1,3}),(\\d{1,3})\\))|do\\(\\)|don't\\(\\)";
+        Pattern p2 = Pattern.compile(rx2);
 
         int sumDel1 = 0;
+        int sumDel2 = 0;
+        boolean canMult = true;
         while (sc.hasNext()) {
             String line = sc.nextLine();
             Matcher matcher = p.matcher(line);
+            Matcher matcher2 = p2.matcher(line);
 
             while (matcher.find()) {
                 int x = Integer.parseInt(matcher.group(1));
@@ -27,32 +33,22 @@ class tredje {
                 //System.out.println(x + " | " + y);
                 sumDel1 += x*y;
             }
-        }
-        System.out.println("Sum1: " + sumDel1);
 
-        rx = "(?:mul\\((\\d{1,3}),(\\d{1,3})\\))|do\\(\\)|don't\\(\\)";
-        p = Pattern.compile(rx);
-
-        int sumDel2 = 0;
-        boolean canMult = true;
-        while (sc.hasNext()) {
-            String line = sc.nextLine();
-            Matcher matcher = p.matcher(line);
-
-            while (matcher.find()) {
-                if (matcher.group(1) != null && matcher.group(2) != null) {
-                    int x = Integer.parseInt(matcher.group(1));
-                    int y = Integer.parseInt(matcher.group(2));
+            while (matcher2.find()) {
+                if (matcher2.group(1) != null && matcher2.group(2) != null) {
+                    int x = Integer.parseInt(matcher2.group(1));
+                    int y = Integer.parseInt(matcher2.group(2));
                     if (canMult) {
                         sumDel2 += x*y;
                     }
-                } else if (matcher.group(0).equals("do()")) {
+                } else if (matcher2.group(0).equals("do()")) {
                     canMult = true;
-                } else if (matcher.group(0).equals("don't()")) {
+                } else if (matcher2.group(0).equals("don't()")) {
                     canMult = false;
                 }
             }
         }
+        System.out.println("Sum1: " + sumDel1);
         System.out.println("Sum2: " + sumDel2);
     }
 }
